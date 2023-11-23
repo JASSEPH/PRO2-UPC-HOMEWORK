aebb6205da4af7d0e1962d7afbcaf7bcaaebcd9b
JASSEPH <149634812+JASSEPH@users.noreply.github.com>
2023-11-23 16:03:56 -0400

    Add files via upload

---
+++ Domino/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Programacion2jass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           //long f = Factorial(64);
+           //Console.WriteLine(f);
+           GenerarSerieV1Recursivo(6);
+        }   
+        static int A = 0;
+        static int B = 7;
+         public static void GenerarSerieV1Recursivo(int N){
+            
+            if(N == 0){
+                return;
+            }else{
+                GenerarSerieV1Recursivo(N - 1); 
+                int serie1 = A + 1;
+                int serie2 = B - 1;
+                Console.WriteLine("Serie: " + serie1);
+                Console.WriteLine("Serie: " + serie2);
+                A = serie1;    
+                B = serie2;
+            }
+        }
+
+
+    }
+}
